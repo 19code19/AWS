be4a0c1a20a0f7e9def0783fb85a52e649fdfd14
biswajitsenapati383 <bswjtsnpt@gmail.com>
2024-10-15 20:55:00 +0530

    Update SampleController.cs as

--- SampleController.cs
+++ SampleController.cs
@@ -1,102 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MultiTenantApi.Api.Commands.Sample.Create;
-using MultiTenantApi.Api.Commands.Sample.Update;
-using MultiTenantApi.Api.Commands.Sample.Delete;
-using MultiTenantApi.Api.Queries.Sample.ListQuery;
-using MultiTenantApi.Api.Queries.Sample.IdQuery;
-using System.Threading.Tasks;
-using MediatR;
-using MultiTenantApi.Api.Models.Sample;
+// Updated block and unblock navigation logic
+let unBlock: any = null;
 
-namespace MultiTenantApi.Api.Controllers
-{
-    [ApiController]
-    [Produces("application/json", "application/xml")]
-    [Route("{tenant}/v{version:apiVersion}/Sample")]
-    public class SampleController : ApiControllerBase
-    {
-        public SampleController(IMediator mediator) : base(mediator)
-        {
-        }
+export const blockNavigation = () => {
+  if (!unBlock) {
+    unBlock = history.block();
+  }
+};
 
-        /// <summary>
-        /// Get all Sample.
-        /// </summary>
-        /// <response code="200">List of Sample returned.</response>
-        /// <response code="401">The user not authenticated.</response>
-        /// <response code="403">The user not authorised.</response>
-        /// <response code="500">System error.</response>
-        [HttpGet(Name = "GetSampleList")]
-        [ProducesResponseType(200, Type = typeof(ApiListResult<SampleModel>))]
-        [ProducesResponseType(401, Type = typeof(ErrorResult))]
-        [ProducesResponseType(403, Type = typeof(ErrorResult))]
-        [ProducesResponseType(500, Type = typeof(ErrorResult))]
-        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
-        public async Task<IActionResult> GetSampleListAsync() =>
-            await Send(new GetSampleListQueryRequest());
+export const unBlockNavigation = () => {
+  if (unBlock) {
+    unBlock();
+    unBlock = null;
+  }
+};
 
-        /// <summary>
-        /// Get a single Sample by id
-        /// </summary>
-        /// <response code="200">Single Sample returned.</response>
-        /// <response code="401">The user not authenticated.</response>
-        /// <response code="403">The user not authorised.</response>
-        /// <response code="500">System error.</response>
-        [HttpGet("{id}", Name = "GetSample")]
-        [ProducesResponseType(200, Type = typeof(SampleModel))]
-        [ProducesResponseType(401, Type = typeof(ErrorResult))]
-        [ProducesResponseType(403, Type = typeof(ErrorResult))]
-        [ProducesResponseType(500, Type = typeof(ErrorResult))]
-        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
-        public async Task<IActionResult> GetSampleByIdAsync(string id) =>
-            await Send(new GetSampleByIdQueryRequest(id));
+// Test cases
+describe('Navigation Blocking', () => {
+  let mockBlock: jest.SpyInstance;
 
-        /// <summary>
-        /// Creates a new Sample object
-        /// </summary>
-        /// <response code="201">Sample object created.</response>
-        /// <response code="401">The user not authenticated.</response>
-        /// <response code="403">The user not authorised.</response>
-        /// <response code="500">System error.</response>
-        [HttpPost(Name = "CreateSample")]
-        [ProducesResponseType(201, Type = typeof(CreateSampleCommandResponse))]
-        [ProducesResponseType(401, Type = typeof(ErrorResult))]
-        [ProducesResponseType(403, Type = typeof(ErrorResult))]
-        [ProducesResponseType(500, Type = typeof(ErrorResult))]
-        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Create))]
-        public async Task<IActionResult> CreateSampleAsync([FromBody] CreateSampleCommandRequest command) =>
-            await Send(command);
+  beforeEach(() => {
+    mockBlock = jest.spyOn(history, 'block').mockImplementation(() => jest.fn());
+    unBlock = null; // Reset unBlock before each test
+  });
 
-        /// <summary>
-        /// Updates a Sample object
-        /// </summary>
-        /// <response code="200">Sample object updated.</response>
-        /// <response code="401">The user not authenticated.</response>
-        /// <response code="403">The user not authorised.</response>
-        /// <response code="500">System error.</response>
-        [HttpPut("{id}", Name = "UpdateSample")]
-        [ProducesResponseType(200, Type = typeof(UpdateSampleCommandResponse))]
-        [ProducesResponseType(401, Type = typeof(ErrorResult))]
-        [ProducesResponseType(403, Type = typeof(ErrorResult))]
-        [ProducesResponseType(500, Type = typeof(ErrorResult))]
-        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Update))]
-        public async Task<IActionResult> UpdateSampleAsync(string id, [FromBody] UpdateSampleCommandRequest command) =>
-            await Send(command);
+  afterEach(() => {
+    jest.clearAllMocks(); // Ensure all mocks are cleared after each test
+  });
 
-        /// <summary>
-        /// Deletes a Sample object
-        /// </summary>
-        /// <response code="204">Sample object deleted.</response>
-        /// <response code="401">The user not authenticated.</response>
-        /// <response code="403">The user not authorised.</response>
-        /// <response code="500">System error.</response>
-        [HttpDelete("{id}", Name = "DeleteSample")]
-        [ProducesResponseType(204)]
-        [ProducesResponseType(401, Type = typeof(ErrorResult))]
-        [ProducesResponseType(403, Type = typeof(ErrorResult))]
-        [ProducesResponseType(500, Type = typeof(ErrorResult))]
-        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Delete))]
-        public async Task<IActionResult> DeleteSampleAsync(string id) =>
-            await Send(new DeleteSampleCommandRequest(id));
-    }
-}
+  test('blockNavigation should call history.block if unBlock is null', () => {
+    blockNavigation();
+    expect(mockBlock).toHaveBeenCalled();
+  });
+
+  test('blockNavigation should not call history.block again if unBlock is not null', () => {
+    blockNavigation(); // First call to set unBlock
+    blockNavigation(); // Second call should not call history.block again
+    expect(mockBlock).toHaveBeenCalledTimes(1);
+  });
+
+  test('unBlockNavigation should call unBlock and set it to null', () => {
+    blockNavigation(); // Set unBlock
+    unBlockNavigation(); // Call unBlock
+    expect(mockBlock.mock.results[0].value).toHaveBeenCalled(); // Check if unBlock is called
+    expect(unBlock).toBeNull(); // Ensure unBlock is null after being called
+  });
+
+  test('unBlockNavigation should not call unBlock if it is null', () => {
+    unBlockNavigation(); // unBlock is already null
+    expect(mockBlock).not.toHaveBeenCalled(); // Should not call history.block
+  });
+});
