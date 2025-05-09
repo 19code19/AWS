49a01d422dafa89fcb02ff8c2fdc7b8030a79060
19code19 <bswjtsnpt@gmail.com>
2025-05-09 15:25:19 +0530

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,70 +1,14 @@
-using OfficeOpenXml;
-using System.Reflection;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace EPPLUS.Excel
+protected override void OnModelCreating(ModelBuilder modelBuilder)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            var people = ReadExcelToList<Person>("Book1.xlsx");
-            Console.WriteLine("Hello, World!");
-        }
+    var employeeConverter = new ValueConverter<Employee, string>(
+        v => JsonSerializer.Serialize(v, new JsonSerializerOptions()),   // Serialize
+        v => JsonSerializer.Deserialize<Employee>(v, new JsonSerializerOptions()) // Deserialize
+    );
 
-        public static List<T> ReadExcelToList<T>(string filePath) where T : new()
-        {
-            FileInfo fileInfo = new(filePath);
-            using ExcelPackage package = new(fileInfo);
-            ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-            if (worksheet.Dimension == null) return [];
-
-            var headers = GetHeaders(worksheet);
-            return [.. Enumerable.Range(2, worksheet.Dimension.End.Row - 1).Select(row => CreateObjectFromRow<T>(worksheet, headers, row))];
-        }
-
-        private static Dictionary<int, string> GetHeaders(ExcelWorksheet worksheet)
-        {
-            return Enumerable.Range(1, worksheet.Dimension.End.Column)
-                             .ToDictionary(
-                                 col => col,
-                                 col => worksheet.Cells[1, col].Value?.ToString()?.Trim() ?? string.Empty
-                             );
-        }
-
-        private static T CreateObjectFromRow<T>(ExcelWorksheet worksheet, Dictionary<int, string> headers, int row) where T : new()
-        {
-            T obj = new T();
-            foreach (var (col, header) in headers)
-            {
-                var property = typeof(T).GetProperty(header, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-                if (property != null && property.CanWrite)
-                {
-                    var cellValue = worksheet.Cells[row, col].Value;
-                    if (cellValue != null)
-                    {
-                        try
-                        {
-                            object convertedValue = Convert.ChangeType(cellValue, property.PropertyType);
-                            property.SetValue(obj, convertedValue);
-                        }
-                        catch
-                        {
-                            // Handle conversion errors silently
-                        }
-                    }
-                }
-            }
-            return obj;
-        }
-    }
-}
-
-
-
-public class Person
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string City { get; set; }
+    modelBuilder.Entity<MyEntity>()
+        .Property(e => e.Employee)
+        .HasConversion(employeeConverter);
 }
