8b45ce004d750739b309418b942ae586cda438ac
19code19 <bswjtsnpt@gmail.com>
2025-07-21 20:14:39 +0530

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,14 +1,45 @@
-using System.Text.Json;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-
-protected override void OnModelCreating(ModelBuilder modelBuilder)
+public static TResult MapTo<TResult>(this object source) where TResult : new()
 {
-    var employeeConverter = new ValueConverter<Employee, string>(
-        v => JsonSerializer.Serialize(v, new JsonSerializerOptions()),   // Serialize
-        v => JsonSerializer.Deserialize<Employee>(v, new JsonSerializerOptions()) // Deserialize
-    );
+    var target = new TResult();
+    var sourceType = source.GetType();
+    var targetType = typeof(TResult);
 
-    modelBuilder.Entity<MyEntity>()
-        .Property(e => e.Employee)
-        .HasConversion(employeeConverter);
+    sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+        .Select(property => new
+        {
+            property,
+            targetProperty = targetType.GetProperty(property.Name)
+        })
+        .Where(x => x.targetProperty != null && x.targetProperty.CanWrite)
+        .ToList()
+        .ForEach(x =>
+        {
+            var sourceValue = x.property.GetValue(source);
+            var sourceTypeProp = x.property.PropertyType;
+            var targetTypeProp = x.targetProperty.PropertyType;
+
+            object? valueToSet = null;
+
+            if (sourceValue == null)
+            {
+                valueToSet = null;
+            }
+            else if (targetTypeProp == sourceTypeProp)
+            {
+                valueToSet = sourceValue;
+            }
+            else if (targetTypeProp.IsEnum && sourceTypeProp.IsEnum)
+            {
+                // Enum to Enum mapping by name
+                valueToSet = Enum.Parse(targetTypeProp, sourceValue.ToString());
+            }
+            else
+            {
+                return; // skip incompatible types
+            }
+
+            x.targetProperty.SetValue(target, valueToSet);
+        });
+
+    return target;
 }
